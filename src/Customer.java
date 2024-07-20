c4d15e0ff8d9eee9357d7ec2c2fd3eda5bcf1934
brian070069 <kkipngeno409@gmail.com>
2024-07-21 02:29:00 +0300

    Constructor challenge

---
+++ src/Customer.java
@@ -0,0 +1,31 @@
+public class Customer{
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public String getName(){
+        return name;
+    }
+
+    public double getCreditLimit(){
+        return creditLimit;
+    }
+
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+
+    public Customer(String name,double creditLimit,String emailAddress){
+        this.name =name;
+        this.creditLimit=creditLimit;
+        this.emailAddress=emailAddress;
+    }
+
+    public Customer(){
+        this("Kev","kev@gmail.com");
+    }
+
+    public Customer(String name,String emailAddress){
+        this(name,500000,emailAddress);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Customer customer1= new Customer("John",900,"John@gmail.com");
+        System.out.println(" name ="+customer1.getName());
+    }
+}
